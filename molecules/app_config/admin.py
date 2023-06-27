0be930ec182ae4743ad72afd2a9f36d6ce376b24
JashKakadiya <jashkakadiya18@gmail.com>
2023-06-27 14:07:59 +0530

    update app config admin

--- molecules/app_config/admin.py
+++ molecules/app_config/admin.py
@@ -3,19 +3,22 @@ from django.contrib import admin
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from .models import *
-import json
+import json,os
+from django.conf import settings
 
 # Register your models here.
 @admin.register(app_config)
 class app_configAdmin(admin.ModelAdmin):
+    
     change_list_template = "pages/admin/app_config/app_config_change_list.html"
     list_display = ("title","store","key","value","update_date")
 
     def changelist_view(self, request: HttpRequest, extra_context=None):
+        base_dir = settings.BASE_DIR
         extra_context = extra_context or {}
 
         # Load JSON data from file
-        with open('C:/Users/jigne/Desktop/django_cms/molecules/app_config/app_config.json') as json_file:
+        with open(os.path.join(base_dir,'molecules','app_config','app_config.json')) as json_file:
             json_data = json.load(json_file)
 
         result = []
