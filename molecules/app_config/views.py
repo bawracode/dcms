43379d8101a4d6345596ea05a0f53711c81db299
jigneshShiyal <190210107071@gecbhavnagar.gujgov.edu.in>
2023-06-28 14:20:40 +0530

    update script.js

--- molecules/app_config/views.py
+++ molecules/app_config/views.py
@@ -3,9 +3,13 @@ from django.http import JsonResponse
 from django.shortcuts import render,HttpResponse
 import importlib
 import json
+import os 
+from django.conf import settings
+
+base_dir = settings.BASE_DIR
 
 def update_value():
-        with open('nucleus\compiled\compiled_config.json') as json_file:
+        with open(os.path.join(base_dir,'nucleus','compiled','compiled_config.json')) as json_file:
             json_data = json.load(json_file)
 
 # update default value
@@ -20,7 +24,7 @@ def update_default_value(data, key, new_value):
             update_default_value(item, key, new_value)
 
 #load json file
-with open('nucleus\compiled\compiled_config.json') as json_file:
+with open(os.path.join(base_dir,'nucleus','compiled','compiled_config.json')) as json_file:
     json_data = json.load(json_file)
 
 # from molecules.test1.source.status import status
@@ -104,7 +108,7 @@ def save_value(request):
 
             update_default_value(json_data, key, value)
 
-        with open('nucleus\compiled\compiled_config.json', 'w') as outfile:
+        with open(os.path.join(base_dir,'nucleus','compiled','compiled_config.json'), 'w') as outfile:
             json.dump(json_data, outfile, indent=4)
 
         update_value()
