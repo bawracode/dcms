40613a3398bd9fdc10bb3710efb17a20ac612197
JashKakadiya <jashkakadiya18@gmail.com>
2023-06-29 14:26:58 +0530

    config file update

--- nucleus/controller/main_controller.py
+++ nucleus/controller/main_controller.py
@@ -5,5 +5,3 @@ class Compilation:
     def __init__(self,file_name):
         self.compiled_dir = os.path.join( Path(__file__).resolve().parent.parent ,'compiled')
         self.apps_file = file_name
-
-        
