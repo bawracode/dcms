4a29525945befedbb06690fcb16c95d90d39724d
JashKakadiya <jashkakadiya18@gmail.com>
2023-07-03 10:13:36 +0530

    cms logs and pages views update

--- molecules/cms/pages/views.py
+++ molecules/cms/pages/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render,HttpResponse
-from django.core.exceptions import ObjectDoesNotExist
-from molecules.cms.block.models import Blocks
-from .models import CustomPage
-import re
-from molecules.cms.pages.models import *
-from molecules.cms.cms_logs.models import *
-
+from django.shortcuts import HttpResponse
 from molecules.cms.block.views import render_block
 
     
